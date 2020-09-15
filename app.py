55976a4bfa887364bef0b6a869c57dcdf52a07c1
Joseph Vargas <joseph@josvar.com>
2020-09-15 02:36:15 -0600

    Mejoras en back-end, mensaje de error si está caído el TD, AES para credenciales

--- app.py
+++ app.py
@@ -1,20 +1,29 @@
 import os
 import datetime
+from base64 import b64encode, b64decode
 import requests
 import jwt
 from bs4 import BeautifulSoup
 from ics import Calendar, Event
 from flask import Flask, request, render_template
+from Crypto.Cipher import AES
+from Crypto.Random import get_random_bytes
 import arrow
 
 # ASEGÚRESE DE CONFIGURAR LA VARIABLE DE ENTORNO 'SECRET' CON UN STRING ALEATORIO CRIPTOGRÁFICAMENTE SEGURO
 SECRET = os.environ.get('SECRET')
 
 
+
 def td_login(username, password):
 
     # Obtiene los tokens de login iniciales
-    initial_request = requests.get('https://tecdigital.tec.ac.cr/register/?return_url=%2fdotlrn%2f')
+    try:
+        initial_request = requests.get('https://tecdigital.tec.ac.cr/register/?return_url=%2fdotlrn%2f', timeout=10)
+    except requests.exceptions.Timeout:
+        raise Exception('El TEC Digital está caído. Por favor inténtelo de nuevo más tarde.')
+    except requests.exceptions.RequestException as e:
+        raise SystemExit(e)
 
     soup = BeautifulSoup(initial_request.content, features='lxml')
 
@@ -29,7 +38,12 @@ def td_login(username, password):
     data = f'form%3Aid=login&return_url=%2Fdotlrn%2F&time={time}&token_id={token_id}&hash={tdhash}&retoken=allow&username={username}&password={password}'
 
     session = requests.Session()
-    session.post('https://tecdigital.tec.ac.cr/register/', data=data, allow_redirects=False)
+    try:
+        session.post('https://tecdigital.tec.ac.cr/register/', data=data, allow_redirects=False, timeout=10)
+    except requests.exceptions.Timeout:
+        raise Exception('El TEC Digital está caído. Por favor inténtelo de nuevo más tarde.')
+    except requests.exceptions.RequestException as e:
+        raise SystemExit(e)
 
     return session
 
@@ -39,7 +53,9 @@ def get_calendar(user, password):
     date = datetime.datetime.today()
     try:
         response = session.get('https://tecdigital.tec.ac.cr/dotlrn/?date=' + date.strftime('%Y-%m-%d') + '&view=list&page_num=1&period_days=90',
-                               allow_redirects=False)
+                               allow_redirects=False, timeout=10)
+    except requests.exceptions.Timeout:
+        raise Exception('El TEC Digital está caído. Por favor inténtelo de nuevo más tarde.')
     except requests.exceptions.RequestException as e:
         raise SystemExit(e)
 
@@ -68,7 +84,7 @@ def get_calendar(user, password):
         e.name = f'{event_data[2]} - {event_data[3]}'
         # HOTFIX: eventos sin descripción
         try:
-            e.description = event_data[4]
+            e.description = event_data[4].replace('Pulse aquí para ir a', 'Puede encontrar más detalles en')
         except IndexError:
             e.description = ""
         date = arrow.get(event_data[0], 'DD MMMM YYYY', locale='es').replace(tzinfo='America/Costa_Rica')
@@ -106,10 +122,20 @@ def create_token():
         user = request.form['user'].strip()
         password = request.form['password'].strip()
 
-        encoded_jwt = jwt.encode({'user': user, 'password': password}, SECRET, algorithm='HS256')
-
         # Intenta obtener el calendario para verificar los datos de inicio de sesión
         get_calendar(user, password)
+
+        iv = get_random_bytes(16)
+        cipher = AES.new(SECRET[0:32].encode('utf-8'), AES.MODE_CFB, iv)
+
+        # Prepara strings seguros
+        user = b64encode(cipher.encrypt(user.encode('utf-8'))).decode('utf-8')
+        password = b64encode(cipher.encrypt(password.encode('utf-8'))).decode('utf-8')
+        iv = b64encode(iv).decode('utf-8')
+
+
+        encoded_jwt = jwt.encode({'user': user, 'password': password, 'iv': iv}, SECRET, algorithm='HS256')
+
 
         return f'https://tdcal.josvar.com/{encoded_jwt.decode("utf-8")}/cal.ics'
 
@@ -124,8 +150,18 @@ def read_calendar(token):
             raise Exception("La variable de entorno SECRET no se ha inicializado.")
 
         data = jwt.decode(token, SECRET, algorithms=['HS256'])
+        if "iv" in data:
+            cipher = AES.new(SECRET[0:32].encode('utf-8'), AES.MODE_CFB, b64decode(data['iv']))
+            user = cipher.decrypt(b64decode(data['user'])).decode('utf-8')
+            password = cipher.decrypt(b64decode(data['password'])).decode('utf-8')
+        else:
+            user = data['user']
+            password = data['password']
 
-        cal = get_calendar(data['user'], data['password'])
+        print(user)
+        print(password)
+
+        cal = get_calendar(user, password)
 
         cal = str(cal).replace('PRODID:ics.py - http://git.io/lLljaA', 'X-WR-CALNAME:TEC Digital')
 
