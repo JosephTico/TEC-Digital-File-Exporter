3af4c77e823c20727caa00b7eaf7f78f5daca536
Restyled.io <commits@restyled.io>
2021-01-06 01:41:55 +0000

    Restyled by autopep8

--- app.py
+++ app.py
@@ -31,7 +31,9 @@ class DownloadProgressBar(tqdm):
 def download_url(url, output_path):
     with DownloadProgressBar(unit='B', unit_scale=True,
                              miniters=1, desc=url.split('/')[-1]) as t:
-        urllib.request.urlretrieve(url, filename=output_path, reporthook=t.update_to)
+        urllib.request.urlretrieve(
+            url, filename=output_path, reporthook=t.update_to)
+
 
 def print_error(msg):
     global globalError
@@ -39,10 +41,12 @@ def print_error(msg):
     print("\n\nERROR: " + msg)
     sys.exit(2)
 
+
 def td_login(username, password):
     global session
     # Obtiene los tokens de login iniciales
-    initial_request = requests.get('https://tecdigital.tec.ac.cr/register/?return_url=%2fdotlrn%2f', timeout=10)
+    initial_request = requests.get(
+        'https://tecdigital.tec.ac.cr/register/?return_url=%2fdotlrn%2f', timeout=10)
 
     try:
         soup = BeautifulSoup(initial_request.content, features='lxml')
@@ -53,16 +57,18 @@ def td_login(username, password):
 
         tdhash = soup.find('input', {'name': 'hash'}).get('value')
     except:
-        print_error('No se ha podido iniciar sesión, el TEC Digital debe estar caído. Por favor inténtelo de nuevo más tarde.')
-
+        print_error(
+            'No se ha podido iniciar sesión, el TEC Digital debe estar caído. Por favor inténtelo de nuevo más tarde.')
 
     # Ahora sí hace el request del login
     data = f'form%3Aid=login&return_url=%2Fdotlrn%2F&time={time}&token_id={token_id}&hash={tdhash}&retoken=allow&username={username}&password={password}'
 
-    session.post('https://tecdigital.tec.ac.cr/register/', data=data, allow_redirects=False, timeout=10)
+    session.post('https://tecdigital.tec.ac.cr/register/',
+                 data=data, allow_redirects=False, timeout=10)
 
     # Verifica login
-    response = session.get('https://tecdigital.tec.ac.cr/dotlrn/courses', allow_redirects=False, timeout=10)
+    response = session.get(
+        'https://tecdigital.tec.ac.cr/dotlrn/courses', allow_redirects=False, timeout=10)
     if response.status_code != 200:
         print_error('Los datos son incorrectos o el TEC Digital está caído.')
 
@@ -74,12 +80,12 @@ def obtener_cursos():
 
     base = "https://tecdigital.tec.ac.cr"
 
-    cursos_page = session.get('https://tecdigital.tec.ac.cr/dotlrn/courses', allow_redirects=True, timeout=10)
-    
+    cursos_page = session.get(
+        'https://tecdigital.tec.ac.cr/dotlrn/courses', allow_redirects=True, timeout=10)
+
     soup = BeautifulSoup(cursos_page.content, features='lxml')
     cursos_html = soup.select_one("#main-content .portlet ul")
     semestres_html = cursos_html.find_all("li")
-
 
     for semestre in tqdm(semestres_html):
         titulo_semestre = semestre.find(text=True, recursive=False).strip()
@@ -93,21 +99,22 @@ def obtener_cursos():
 
         for curso in cursos_soup:
             titulo_curso = curso.text.strip()
-            url = base + curso.find("a", href=True)['href'].strip() + "file-storage"
+            url = base + \
+                curso.find("a", href=True)['href'].strip() + "file-storage"
 
             folder_page = session.get(url, allow_redirects=True, timeout=10)
             regex = re.compile(r"\$rootScope\.GL_FOLDER_ID = ([0-9]*);")
-            folder_id = re.search(regex, folder_page.content.decode('utf-8')).group(1)
+            folder_id = re.search(
+                regex, folder_page.content.decode('utf-8')).group(1)
 
-
-            cursos.append({"titulo": titulo_curso, "url": url, "folder_id": folder_id})
-
+            cursos.append(
+                {"titulo": titulo_curso, "url": url, "folder_id": folder_id})
 
         data_semestre = {"titulo": titulo_semestre, "cursos": cursos}
         semestres_final.append(data_semestre)
-        
 
-# Obtenido de https://stackoverflow.com/questions/3041986/apt-command-line-interface-like-yes-no-input     
+
+# Obtenido de https://stackoverflow.com/questions/3041986/apt-command-line-interface-like-yes-no-input
 def query_yes_no(question, default="yes"):
     """Ask a yes/no question via raw_input() and return their answer.
 
@@ -141,7 +148,6 @@ def query_yes_no(question, default="yes"):
                              "(o 'y' o 'n').\n")
 
 
-
 def cli_login():
     global session, globalError, semestres_final
     art = """
@@ -161,23 +167,21 @@ _____ __     __   __ ___           ___    __  __  ________ __
         password = environ.get('PASSWORD')
     else:
         password = getpass.getpass("Contraseña: ")
-   
 
     spinner = PixelSpinner('Iniciando sesión... ')
 
-    thread = threading.Thread(target=td_login,args=(username,password))
+    thread = threading.Thread(target=td_login, args=(username, password))
     thread.start()
 
     while thread.is_alive() and globalError == False:
         spinner.next()
-    
+
     thread.join()
 
     if globalError:
         return
 
     print("\n")
-
 
     print('Obteniendo cursos... ')
 
@@ -204,22 +208,25 @@ _____ __     __   __ ___           ___    __  __  ________ __
     if "AUTO_DOWNLOAD" not in environ and not query_yes_no("¿Desea iniciar la descarga de todos los archivos en la carpeta actual?"):
         return
 
-
     for semestre in semestres_final:
         print("Descargando cursos de " + semestre["titulo"] + "...")
-        
+
         if not os.path.exists(semestre["titulo"]):
             os.makedirs(semestre["titulo"])
 
         for curso in semestre["cursos"]:
             print("Descargando archivos de " + curso["titulo"] + "...")
 
-            url = curso["url"] + "/download-archive?object_id=" + curso["folder_id"]
+            url = curso["url"] + "/download-archive?object_id=" + \
+                curso["folder_id"]
             response = session.get(url, stream=True)
-            total_size_in_bytes= int(response.headers.get('content-length', 0))
-            block_size = 1024 #1 Kibibyte
-            progress_bar = tqdm(total=total_size_in_bytes, unit='iB', unit_scale=True)
-            filename = os.path.join(dirname, semestre["titulo"], curso["titulo"] + ".zip")
+            total_size_in_bytes = int(
+                response.headers.get('content-length', 0))
+            block_size = 1024  # 1 Kibibyte
+            progress_bar = tqdm(total=total_size_in_bytes,
+                                unit='iB', unit_scale=True)
+            filename = os.path.join(
+                dirname, semestre["titulo"], curso["titulo"] + ".zip")
             with open(filename, 'wb') as file:
                 for data in response.iter_content(block_size):
                     progress_bar.update(len(data))
@@ -227,15 +234,12 @@ _____ __     __   __ ___           ___    __  __  ________ __
             if total_size_in_bytes != 0 and progress_bar.n != total_size_in_bytes:
                 print_error("Ha ocurrido un error al descargar el archivo.")
 
-            with zipfile.ZipFile(filename,"r") as zip_ref:
+            with zipfile.ZipFile(filename, "r") as zip_ref:
                 zip_ref.extractall(os.path.join(dirname, semestre["titulo"]))
 
             os.remove(filename)
 
             progress_bar.close()
-
-
-
 
         print("\n")
         print("Proceso finalizado.")
